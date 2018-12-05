9f5cf47f61b57e7b388e7fba3ba08d1616149dce
tsejx <tsejx@foxmail.com>
2018-12-05 14:46:32 +0800

    Modified comment info

--- src/es/guid.js
+++ src/es/guid.js
@@ -1,11 +1,13 @@
-/* * @Author: tsejx  * @Date: 2018-12-01 11:58:05  * @Last Modified by:   tsejx  * @Last Modified time: 2018-12-01 11:58:05 */
+/*
+ * @Author: tsejx 
+ * @Date: 2018-12-04 17:06:36 
+ * @Last Modified by: tsejx
+ * @Last Modified time: 2018-12-04 17:07:37
+ */
 
 // GUID 全局唯一标识符
 // Generate four random hex digits.
-function S4() {
-    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-}
+const S4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+
 // Generate a pseudo-GUID by concatenating random hexadecimal.
-function guid() {
-    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-}
+const guid = () => (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
