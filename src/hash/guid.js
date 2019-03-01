d1f2bf5d3425d19e22caff9ea5a5a2586f8f55c3
tsejx <tsejx@foxmail.com>
2019-03-01 15:07:15 +0800

    Add guid method

---
+++ src/hash/guid.js
@@ -0,0 +1,12 @@
+// GUID 全局唯一标识符
+// Generate four random hex digits.
+function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
+// Generate a pseudo-GUID by concatenating random hexadecimal.
+function guid() {
+    return S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4();
+}
+
+export default guid;
