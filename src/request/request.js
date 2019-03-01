c7de8f988fec14c510a42f28d230502421154012
tsejx <tsejx@foxmail.com>
2019-03-01 17:57:36 +0800

    Add request methods

--- src/request/request.js
+++ src/request/request.js
@@ -1,5 +1,4 @@
-const ajax = {};
-ajax.httpRequest = () => {
+function httpRequest() {
     //判断是否支持XMLHttpRequest对象
     // Chrome, Firefox, Opera 8.0+, Safari
     if (window.XMLHttpRequest) {
@@ -7,12 +6,12 @@ ajax.httpRequest = () => {
     }
     //兼容IE浏览器
     const versions = [
-        "MSXML2.XmlHttp.6.0",
-        "MSXML2.XmlHttp.5.0",
-        "MSXML2.XmlHttp.4.0",
-        "MSXML2.XmlHttp.3.0",
-        "MSXML2.XmlHttp.2.0",
-        "Microsoft.XmlHttp"
+        'MSXML2.XmlHttp.6.0',
+        'MSXML2.XmlHttp.5.0',
+        'MSXML2.XmlHttp.4.0',
+        'MSXML2.XmlHttp.3.0',
+        'MSXML2.XmlHttp.2.0',
+        'Microsoft.XmlHttp',
     ];
     //定义局部变量xhr,储存IE浏览器的ActiveXObject对象
     let xhr;
@@ -23,14 +22,34 @@ ajax.httpRequest = () => {
         } catch (e) {}
     }
     return xhr;
-};
+}
 
-ajax.send = (url, callback, method, data, async) => {
+/**
+ * 发起HTTP请求
+ * @param  {any} url 请求地址
+ * @param  {any} method 仅支持GET和POST方法
+ * @param  {any} data 请求参数
+ * @param  {any} callback 回调函数
+ * @param  {any} async 异步请求
+ * @return {void}
+ */
+function request(url, method, data, callback, async) {
+    const query = [];
+
+    for (let key in data) {
+        query.push(`${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`);
+    }
+
+    let params = method === 'GET' ? null : query.join('&');
+
+    if (method === 'GET') {
+        url = `${url}${query.length ? `?${query.join('&')}` : ''}`;
+    }
+
     //默认异步
     if (async === undefined) {
         async = true;
     }
-    let httpRequest = ajax.httpRequest();
     //初始化HTTP请求
     httpRequest.open(method, url, async);
     //onreadystatechange函数对象
@@ -38,7 +57,7 @@ ajax.send = (url, callback, method, data, async) => {
         //readyState 的值等于4，从服务器拿到了数据
         if (httpRequest.readyState == 4) {
             //回调服务器响应数据
-            callback(httpRequest.responseText)
+            callback(httpRequest.responseText);
         }
     };
     if (method == 'POST') {
@@ -46,21 +65,7 @@ ajax.send = (url, callback, method, data, async) => {
         httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     }
     //发送HTTP请求
-    httpRequest.send(data);
-};
-//实现GET请求
-ajax.get = (url, data, callback, async) => {
-    const query = [];
-    for (let key in data) {
-        query.push(`${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`);
-    }
-    ajax.send(`${url}${query.length ? `?${query.join('&')}` : ''}`, callback, 'GET', null, async);
-};
-//实现POST请求
-ajax.post = (url, data, callback, async) => {
-    const query = [];
-    for (let key in data) {
-        query.push(`${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`);
-    }
-    ajax.send(url, callback, 'POST', query.join('&'), async);
-};
+    httpRequest.send(params);
+}
+
+export default request;
