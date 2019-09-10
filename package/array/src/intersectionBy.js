fd6ff737d74d886f4e99d3d0b798e7b953d9b676
tsejx <tsejx@foxmail.com>
2019-09-10 23:54:46 +0800

    :sparkles: feat:add new array methods including intersectionBy and intersectionWith

---
+++ package/array/src/intersectionBy.js
@@ -0,0 +1,17 @@
+/**
+ * 数组交集函数(ES6+)
+ * @param {array} a 需要处理的数组
+ * @param {array} b 需要处理的数组
+ * @param {function} fn 用于各自独立筛选成员的筛选器
+ * @returns {array} 返回交集数组
+ * @example
+ *
+ * intersectionBy([2.1, 1.2], [2.3, 3.4], Math.floor)
+ * // => [2.1]
+ */
+function intersectionBy(a, b, fn) {
+    const s = new Set(b.map(fn));
+    return a.filter(x => s.has(fn(x)));
+}
+
+export default intersectionBy;
